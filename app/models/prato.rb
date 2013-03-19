0bf711fc25661f08cdacabcc8e2394e00d289149
beregueder <beregueder@gmail.com>
2013-03-19 15:50:23 -0300

    Model's e migrate

--- app/models/prato.rb
+++ app/models/prato.rb
@@ -1,6 +1,9 @@
 class Prato < ActiveRecord::Base
   attr_accessible :nome
-  
+
   validates_presence_of :nome, message: " - deve ser preenchido"
   validates_uniqueness_of :nome, message: " - nome já cadastrado"
+
+  has_and_belong_to_many :restaurantes
+  has_one :receita
 end
--- app/models/receita.rb
+++ app/models/receita.rb
@@ -1,5 +1,6 @@
 class Receita < ActiveRecord::Base
   attr_accessible :prato_id, :conteudo
   validates_presence_of :conteudo, message: " - deve ser preenchido"
-    
+  belongs_to :prato
+
 end
